8ee6f9788925b4455aa475867d4787df6a289ca2
Nlisi <nlisimalaba@gmail.com>
2025-04-24 17:30:21 +0200

    Remove unnecessary info alert about blood pressure values in the Prediction component to streamline the user interface and improve clarity.

--- my-react-app/src/pages/Prediction.js
+++ my-react-app/src/pages/Prediction.js
@@ -238,25 +238,7 @@ const Prediction = () => {
 
           <form onSubmit={handleSubmit} className="p-6">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              {/* Info alert about BP values */}
-              <div className="mb-6 p-4 bg-blue-50 border border-blue-200 rounded-md">
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <svg className="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                    </svg>
-                  </div>
-                  <div className="ml-3">
-                    <h3 className="text-sm font-medium text-blue-800">Blood Pressure Values</h3>
-                    <div className="mt-2 text-sm text-blue-700">
-                      <p>
-                        Blood pressure and heart rate values will be automatically calculated using the average of your recent measurements from the BP Tracker. 
-                        No need to enter them manually.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
+             
 
               {/* Health Metrics Section */}
               <div className="md:col-span-2">
